782b4a33f4e78959c9fd56ae86a46d628e4b1da6
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-13 11:49:25 -0700

    using prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'this program prints the
+ * strings in quote.'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
