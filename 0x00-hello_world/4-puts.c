fac27a691c68dfdd6940ff0229afb3cead5a628b
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-13 11:17:21 -0700

    using C to print a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n", "0")
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'this program prints the
+ * strings in quotes.'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
