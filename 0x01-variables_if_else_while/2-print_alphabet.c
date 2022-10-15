893e983a6f6313553e4436f28c0ad6771b282c14
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-15 07:17:44 -0700

    printing lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'what the program those
+ * in quotes.'
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a' ; low <= 'z' ; low++)
+	{
+		putchar(low);
+		putchar('\n');
+	}
+	return (0);
+}
