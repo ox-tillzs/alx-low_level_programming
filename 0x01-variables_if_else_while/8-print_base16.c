66f848290eeef9688a64e134904d8bbbf69e33e3
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-16 14:39:25 -0700

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main Entry point
+ * Description: 'what does the program print
+ * in quotes.'
+ * Return: Always n0 (success)
+ */
+
+int main(void)
+{
+	int num;
+	char low;
+
+	for (num = '0' ; num <= '9' ; num++)
+	{
+		putchar(num);
+	}
+	for (low = 'a' ; low <= 'f' ; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
