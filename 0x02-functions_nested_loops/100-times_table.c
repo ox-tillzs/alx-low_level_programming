5c241d8621636e509b7d04598d17ebe73f73844a
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-20 15:59:32 -0700

    creating directort

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,25 +19,25 @@ void print_times_table(int n)
 	for (b = 1; b <= n; b++)
 	{
 		op = a * b;
-		_putchar('0');
-		_putchar('0');
+		_putchar(44);
+		_putchar(32);
 		if (op <= 9)
 		{
-		_putchar('0');
-		_putchar('0');
-		_putchar(op + '0');
+		_putchar(32);
+		_putchar(32);
+		_putchar(op + 48);
 		}
 		else if (op <= 99)
 		{
-		_putchar('0');
-		_putchar((op / 10) + '0');
-		_putchar((op % 10) + '0');
+		_putchar(32);
+		_putchar((op / 10) + 48);
+		_putchar((op % 10) + 48);
 		}
 		else
 		{
-		_putchar(((op / 100) % 10) + '0');
-		_putchar(((op / 10) % 10) + '0');
-		_putchar((op % 10) + '0');
+		_putchar(((op / 100) % 10) + 48);
+		_putchar(((op / 10) % 10) + 48);
+		_putchar((op % 10) + 48);
 		}
 	}
 	_putchar('\n');
---
+++ README.md
@@ -0,0 +1,1 @@
+This is a directory created for learning introduction to C on ALX
