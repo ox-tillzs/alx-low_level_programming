8b6de8110133adaba90468cf55a75dd6c8320e5b
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-20 11:33:12 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - addition
+*@a: first interger placement
+*@b: second interger placement
+*Return: the addition of bothe intergers
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
