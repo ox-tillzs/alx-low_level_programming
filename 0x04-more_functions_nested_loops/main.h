e3e2001708e5f20a19c6eb270b4d916147024b13
ox-tillzs <toluwaboriayomide1@gmail.com>
2022-10-21 16:14:41 -0700

    4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int_putchar(char);
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month,int day);
-void print_remaining_days(int month,int day,int year);
-void print_to_98(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
